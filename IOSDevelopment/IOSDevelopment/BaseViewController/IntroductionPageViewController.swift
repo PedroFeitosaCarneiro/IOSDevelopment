3e426b5fe03f4a58983edd5e129483e352f8d04d
Pedro Feitosa <pedrofeitosa@192.168.100.3>
2025-03-20 11:22:26 -0300

    TopMenu renamed, removed unused files

---
+++ IOSDevelopment/IOSDevelopment/BaseViewController/IntroductionPageViewController.swift
@@ -0,0 +1,12 @@
+//
+//  IntroductionPageViewController.swift
+//  IOSDevelopment
+//
+//  Created by Pedro Feitosa on 17/03/25.
+//
+
+import UIKit
+
+public final class IntroductionPageViewController: UIPageViewController {
+    
+}
